0dc1b8172776a194e8f5b40c64cf7cb9be81bb5a
YuriiYurchuk <urthyk1234@gmail.com>
2025-12-29 20:14:50 +0200

    feat: add syncer for periodic listening history sync

--- app/[lang]/dashboard/layout.tsx
+++ app/[lang]/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { BackgroundSyncer } from '@/components/dashboard/background-syncer';
 import { DashboardHeader } from '@/components/layout/dashboard-header';
 import { Sidebar } from '@/components/layout/sidebar';
 
@@ -10,6 +11,7 @@ export default function DashboardLayout({
 		<div className="flex h-screen overflow-hidden">
 			<Sidebar />
 			<div className="flex-1 flex flex-col overflow-hidden relative">
+				<BackgroundSyncer />
 				<DashboardHeader />
 				<main className="flex-1 overflow-auto p-4 lg:p-6">{children}</main>
 			</div>
