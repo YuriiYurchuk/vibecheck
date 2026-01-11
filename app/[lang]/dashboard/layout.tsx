b4d3ddc15731659fd742eec7d45fea5a89d4a8aa
YuriiYurchuk <urthyk1234@gmail.com>
2026-01-11 02:38:15 +0200

    refactor: replace main tag with div wrapper

--- app/[lang]/dashboard/layout.tsx
+++ app/[lang]/dashboard/layout.tsx
@@ -18,9 +18,9 @@ export default async function DashboardLayout({
 			<SidebarInset>
 				<BackgroundSyncer />
 				<DashboardHeader />
-				<main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+				<div className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
 					{children}
-				</main>
+				</div>
 			</SidebarInset>
 		</SidebarProvider>
 	);
